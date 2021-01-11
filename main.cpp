2c1b73a175ddba80177f589723a7e0ef1b09790a
Jakub <jakub.jakubecky123@gmail.com>
2021-01-11 11:53:57 +0100

    directions to fix

--- main.cpp
+++ main.cpp
@@ -24,93 +24,30 @@ struct AVLTree{
     int treeSize = 0;
     vector<BinaryNode *> testContainer;
 
-    /*void insert(int index, int value){
-        if(root == nullptr){
-            root = new BinaryNode(0);
-            root->value = value;
-            ++ treeSize;
-            testContainer.push_back(root);
-            root->levelInTree = 0;
-            return;
-        }
-        vector<pair<BinaryNode *, pair<bool, int>>> postOrderStack;
-        pair<BinaryNode *, bool> p = insert(root, index, postOrderStack);
-        if(p.second){
-            BinaryNode * left = new BinaryNode(treeSize);
-            left->parent = p.first;
-            left->value = value;
-            ++ treeSize;
-            p.first->left = left;
-            left->levelInTree = p.first->levelInTree + 1;
-
-            testContainer.push_back(left);
-        }else{
-            BinaryNode * right = new BinaryNode(treeSize);
-            right->parent = p.first;
-            right->value = value;
-            ++ treeSize;
-            p.first->right = right;
-            right->levelInTree = p.first->levelInTree + 1;
-
-            testContainer.push_back(right);
-        }
-        for(auto pair : postOrderStack){
-            BinaryNode * n = pair.first;
-            bool left = pair.second.first;
-            left ? ++n->leftSubtreeSize : ++n->rightSubtreeSize;
-            if(pair.second.first){
-                if(n->leftHeight < pair.second.second){
-                    n->leftHeight = pair.second.second;
-                }
-            }
-            else{
-                if(n->rightHeight < pair.second.second){
-                    n->rightHeight = pair.second.second;
-                }
-            }
-            cout<<"";
-        }
-        for(auto it = postOrderStack.rbegin(); it != postOrderStack.rend(); ++it){
-            BinaryNode * n = it->first;
-            ///todo rotate to fix
-            int type;
-            auto tmpIT = it;
-            if(tmpIT->second.first){
-                //L
-                type = 0;
-                if((++ tmpIT) != postOrderStack.rend()){
-                    if(!tmpIT->second.first){
-                        type = 1;
-                        //LR
-                    }
-                }
-            }
-            else{
-                //R
-                type = 2;
-                if((++ tmpIT) != postOrderStack.rend()){
-                    if(tmpIT->second.first){
-                        type = 4;
-                        //LR
-                    }
-                }
-            }
-            rotation(n, type);
-        }
-        //todo test the rotation type
-        cout<<"";
-    }
-*/
     int getValue(int index){
         stack<pair<BinaryNode *, bool>> postOrderStack;
         BinaryNode * p = getNodeAtIndex(root, index, postOrderStack);
         return (p->value);
     }
     void showStructure(){
+        cout<<"\n";
         for(auto n : testContainer){
-            cout<<n->id<<" = "<<n->leftSubtreeSize<<" | "<<n->rightSubtreeSize<<"\n";
+            cout<<n->id<<" subtrees = "<<n->leftSubtreeSize<<" | "<<n->rightSubtreeSize<<"\n";
+            cout<<" heights = "<<n->leftHeight<<" | "<<n->rightHeight<<"\n\n";
         }
         cout<<"\n\n\n\n\n\n";
+    }
+    bool findUnbalanced(){
+        cout<<"\n";
+        bool found = false;
+        for(auto n : testContainer){
+            if(abs(n->leftHeight - n->rightHeight) > 1) {
+                cout << n->id << " subtrees = " << n->leftSubtreeSize << " | " << n->rightSubtreeSize << "\n";
+                cout << " heights = " << n->leftHeight << " | " << n->rightHeight << "\n\n";
+                found  = true;
+            }
+        }
+        return found;
     }
     int max(int x, int y){
         return x > y ? x : y;
@@ -127,123 +64,73 @@ struct AVLTree{
             BinaryNode * newNode = new BinaryNode(treeSize);
             newNode->value = value;
             insert(root, index, newNode, directions);
-            testContainer.push_back(newNode);
             ++ treeSize;
+            testContainer.push_back(newNode);
         }
     }
 private:
-    void insert(BinaryNode * node, int index, BinaryNode * insertedNode, vector<bool> &directions){//node, <left/right, reachableHeight
+    void insert(BinaryNode * node, int index, BinaryNode * insertedNode, vector<bool> &directions, bool forceUnbalanced = false){//node, <left/right, reachableHeight
         bool turnedLeft = true;
         if(index > treeSize ){
             cout<<"INVALID ENTRY!";
             return;
         }
         if(index <= node->leftSubtreeSize){
-            node->leftSubtreeSize ++;
 
             if(node->left == nullptr) {
                 node->left = insertedNode;
                 insertedNode->parent = node;
-                max(insertedNode->leftHeight, insertedNode->rightHeight) + 1;
+                updateNode(node);
             }
             else {
-                insert(node->left, index, insertedNode, directions);
+                insert(node->left, index, insertedNode, directions, forceUnbalanced);
             }
         }
         else {
-            node->rightSubtreeSize ++;
-
             if(node->right == nullptr) {
                 node->right = insertedNode;
                 insertedNode->parent = node;
-                max(insertedNode->leftHeight, insertedNode->rightHeight) + 1;
+                updateNode(node);
             }
-            else
-                insert(node->right, index - node->leftSubtreeSize - 1, insertedNode, directions);
+            else {
+                insert(node->right, index - node->leftSubtreeSize - 1, insertedNode, directions, forceUnbalanced);
+            }
             turnedLeft = false;
         }
         updateNode(node);
         directions.push_back(turnedLeft);
 
-        //time to check for imbalances
         int balance = node->leftHeight - node->rightHeight;
-        if(abs(balance) > 1){
-            int imbalanceType;
-            if(directions.size() >= 2){
-                int nodesIndex = directions.size() - 1;
-                if(directions[nodesIndex - 1]){
-                    //LL path
-                    imbalanceType = 0;
-                    if(directions.size() >= 3){
-                        if(!directions[nodesIndex - 2]) {
-                            imbalanceType = 1;
-                            //LR path
-                        }
-                    }
-                }else{
-                    imbalanceType = 2;
-                    //RR path
-                    if(directions.size() >= 3){
-                        if(directions[nodesIndex - 2]) {
-                            imbalanceType = 3;
-                            //RL path
-                        }
-                    }
-                }
-            }
-            if(abs(balance) > 2){
-                cout<<"WTF\n";
-            }
-            switch(imbalanceType){
-                    case 0:
-                        rightRotation(node);
-                        break;
-                    case 1:
-                        rightRotation(node->left);
-                        leftRotation(node);
-                        break;
-                    case 2:
-                        leftRotation(node);
-                        break;
-                    case 3:
-                        leftRotation(node->right);
-                        rightRotation(node);
-                        break;
-            }
-        }
+        //if negative - left rotation as right is too big
 
+        if(abs(balance) > 1 && !forceUnbalanced){
+            int currDirectionIndex = directions.size() - 1;
+            bool first = directions[currDirectionIndex ];
+            bool second = first;
+            if(directions.size() >= 2)
+                second = directions[currDirectionIndex - 1];
+            if(first && second) {
+                //ll if too heavy
+                leftRotation(node);
+            }
+            else if(first && !second){
+                //lr if too heavy
+                rightRotation(node->left);
+                leftRotation(node);
+            }
+            else if(!first && !second){
+                //rr if too heavy
+                rightRotation(node);
+            }
+            else if(!first && second){
+                //rl if too heavy
+                leftRotation(node->right);
+                rightRotation(node);
+            }
+            cout<<"";
+        }
     }
     void rightRotation(BinaryNode * node){
-        BinaryNode * parent = node->parent;
-        BinaryNode * leftChild = node->left;
-
-        if(parent != nullptr){
-            (parent->left == node ? parent->left = leftChild : parent->right = leftChild);
-        }
-        node->parent = leftChild;
-        node->left = nullptr;
-        node->leftHeight = 0;
-        node->leftSubtreeSize = 0;
-
-        if(leftChild->right != nullptr) {
-            vector<bool> dirs;
-            insert(node, 0, leftChild->right, dirs);
-        }
-        leftChild->right = node;
-        leftChild->parent = parent;
-
-
-        updateNode(node);
-        updateNode(leftChild);
-        if(parent != nullptr)
-            updateNode(parent);
-
-        if(node == root){
-            root = leftChild;
-        }
-        cout<<"";
-    }
-    void leftRotation(BinaryNode * node){
         BinaryNode * parent = node->parent;
         BinaryNode * rightChild = node->right;
 
@@ -252,29 +139,49 @@ private:
         }
         node->parent = rightChild;
         node->right = nullptr;
-        node->rightHeight = 0;
-        node->rightSubtreeSize = 0;
+        updateNode(node);
 
-        if(rightChild->left != nullptr) {
+        if(rightChild->left != nullptr){
             vector<bool> dirs;
-            insert(node, node->leftHeight + 1, rightChild->left, dirs);
+            insert(rightChild->left, rightChild->leftSubtreeSize, node, dirs);
+        }else {
+            rightChild->left = node;
         }
-        rightChild->left = node;
+        updateNode(rightChild);
+
         rightChild->parent = parent;
 
-        updateNode(node);
-        updateNode(rightChild);
-        if(parent != nullptr)
-            updateNode(parent);
-
-        if(node == root){
+        if(node == root)
             root = rightChild;
+        cout<<"";
+    }
+    void leftRotation(BinaryNode * node){
+        BinaryNode * parent = node->parent;
+        BinaryNode * leftChild = node->left;
+
+        if(parent != nullptr){
+            (parent->left == node ? parent->left = leftChild : parent->right = leftChild);
         }
+        node->parent = leftChild;
+        node->left = nullptr;
+        updateNode(node);
+
+        if(leftChild->right != nullptr){
+            vector<bool> dirs;
+            insert(leftChild->right, 0, node, dirs);
+        }else {
+            leftChild->right = node;
+        }
+        updateNode(leftChild);
+        leftChild->parent = parent;
+        if(node == root)
+            root = leftChild;
+        cout<<"";
     }
     void updateNode(BinaryNode * n){
         int lH = 0, lT = 0;
         if(n->left != nullptr) {
-            lH++, lT++;
+            lH = 1, lT = 1;
             lH += max(n->left->leftHeight, n->left->rightHeight);
             lT += n->left->leftSubtreeSize + n->left->rightSubtreeSize;
         }
@@ -283,7 +190,7 @@ private:
 
         int rH = 0, rT = 0;
         if(n->right != nullptr) {
-            rH++, rT++;
+            rH = 1, rT = 1;
             rH += max(n->right->leftHeight, n->right->rightHeight);
             rT += n->right->leftSubtreeSize + n->right->rightSubtreeSize;
         }
@@ -325,12 +232,20 @@ private:
 
 int main() {
     AVLTree avlTree;
-    avlTree.insert(0, 1);
-    avlTree.insert(1, 2);
-    avlTree.insert(2, 3);
-    avlTree.insert(3, 4);
-    avlTree.insert(4, 5);
-    avlTree.insert(5, 6);
+    for(int i = 0; i < 6; i ++) {
+        avlTree.insert(0, i);
+        if(avlTree.findUnbalanced()) {
+            cout<<"";
+            break;
+        }
+    }
+    for(int i = 0; i < 0; i +=3) {
+        avlTree.insert(i, -1);
+        if(avlTree.findUnbalanced()) {
+            cout<<"";
+            break;
+        }
+    }
     cout<<"";
    for(int i = 0; i < avlTree.treeSize; i++){
        cout<<avlTree.getValue(i)<<" ";
